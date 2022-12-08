f94c962c687cff7f38691cfc68d143b2625e56b7
mmmonles <yenlin@jolacare.com>
2022-12-08 09:46:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ app.use(bodyParser.json());
 
 app.get('/test',function(req,res){
    console.log('test');
-   res.send(200,'success');
+   res.status(200).send('success') 
 });
 
 //Setting up server
- var server = app.listen(process.env.PORT || 443, function () {
+ var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
  });
