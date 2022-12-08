18409d18f818d3b1aa5ee1420ec4a332ef022d55
mmmonles <yenlin@jolacare.com>
2022-12-08 08:50:37 +0800

    Update app.js
    aaa

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var app = express();
 // Body Parser Middleware
 app.use(bodyParser.json()); 
 
+app.get('/test',function(req,res){
+   console.log('test');
+   res.send(200,'success');
+});
+
 //Setting up server
  var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
